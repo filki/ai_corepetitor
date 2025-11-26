fb2116ffd827bdfc89566db048fe4e3cb9857803
filki <83698@student.ue.poznan.pl>
2025-11-26 16:44:33 +0100

    fixing bug on p[roductionm

--- app.py
+++ app.py
@@ -5,23 +5,6 @@ from tutor_service import TutorService
 
 # site configuration
 st.set_page_config(page_title="Twój Prywatny Nauczyciel Matmy", page_icon="🧮")
-
-# authentications
-AuthService.require_auth(st.secrets["APP_PASSWORD"])
-
-st.title("🧮 Twój Prywatny Nauczyciel Matmy")
-
-# returns tutor service instance
-@st.cache_resource
-def get_tutor_service():
-    return TutorService(api_key=st.secrets["GOOGLE_API_KEY"])
-tutor_service = get_tutor_service()
-
-if "messages" not in st.session_state:
-    st.session_state.messages = []
-
-with st.sidebar:
-    st.header("📸 Materiały")
     uploaded_file = st.file_uploader("Wgraj zdjęcie zadania", type=['png', 'jpg', 'jpeg'])
     
     current_image = None
